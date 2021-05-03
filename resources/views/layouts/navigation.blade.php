47a5714144afc16a348c237db3ec0627da48e4e7
Gabael Páez <gabael.paez@gmail.com>
2021-05-02 21:03:46 -0700

    Fix log in styling

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,7 +34,7 @@
                     </button>
                 </form>
                 @else
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="hidden sm:block">
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
                 </div>
                 @endauth
